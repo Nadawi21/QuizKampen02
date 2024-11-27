e692b1d283058fa0797a5819c481762b8888d3a2
Patrik Wennerkvist <patrik.wennerkvist@outlook.com>
2024-11-27 20:56:41 +0100

    la till lite kommentarer

--- src/Server.java
+++ src/Server.java
@@ -22,10 +22,10 @@ public class Server {
         try(ServerSocket serverSocket = new ServerSocket(port)){
             while (activeServer) {
                 Socket clientSocket = serverSocket.accept();
-                System.out.println("En ny klient har ansultit: ");
+                System.out.println("En ny klient har ansultit");
 
-                ServerThread cleintThread = new ServerThread(clientSocket, this);
-                listOfClients.add(cleintThread);
+                ServerThread cleintThread = new ServerThread(clientSocket, this);//This för att koppla till denna instans av server
+                listOfClients.add(cleintThread); //En lista för att hålla koll på vilka klienter som har anslutit
                 new Thread(cleintThread).start(); //Skapar upp en ny tråd för varje ansluten klient
 
             }
