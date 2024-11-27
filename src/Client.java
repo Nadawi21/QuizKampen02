abeae4cbd7c15f0ecf0a8ac4a02ba4f4fd9c71e7
Oskar Høxbroe <oskar.hoxbroe@gmail.com>
2024-11-27 19:58:45 +0100

    Liten fix

--- src/Client.java
+++ src/Client.java
@@ -5,7 +5,6 @@ public class Client {
 
     public static void main(String[] args) {
 
-
         try (Socket socket = new Socket("127.0.0.1", 8091);) {
             // Skapar ny klient-tråd
             ClientThread thread = new ClientThread((socket));   //Sends socket to ClientThread  to play game
